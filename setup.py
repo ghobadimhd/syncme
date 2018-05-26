a6cce8469fd699977059a0dcb1059de7597697d0
mamad <ghobadimhd@gmail.com>
2018-05-26 19:24:53 +0430

    Bug fix

--- setup.py
+++ setup.py
@@ -1,16 +1,16 @@
 #!/usr/bin/env python3
 
-from distutils.core import setup
+from setuptools import setup
 
 setup(name='syncme',
-      version='0.1.0',
+      version='0.1.3',
       description='Simple rsync wrapper',
       keywords='sample setuptools development',
       author='Mohammad Ghobadi',
       author_email='ghobadimhd@gmail.com',
       url="https://github.com/ghobadimhd/syncme",
-      python_requires='3.5.*',
-      install_requires=['pyaml'],
+      python_requires='==3.5.*',
+      install_requires=['pyaml==17.*'],
       py_modules=['syncme'],
       entry_points={
           'console_scripts': [
@@ -23,6 +23,5 @@ setup(name='syncme',
           'Topic :: Communications :: File Sharing',
           'License :: OSI Approved :: MIT License',
           'Programming Language :: Python :: 3.5',
-          'Programming Language :: Python :: 3.6',
       ],
 )
