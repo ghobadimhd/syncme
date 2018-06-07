236e433f724450e5b6774d2843b47bda434525d8
mamad <ghobadimhd@gmail.com>
2018-06-07 21:00:49 +0430

    support python3.6

--- setup.py
+++ setup.py
@@ -3,13 +3,13 @@
 from setuptools import setup
 
 setup(name='syncme',
-      version='0.1.3',
+      version='0.1.4',
       description='Simple rsync wrapper',
       keywords='sample setuptools development',
       author='Mohammad Ghobadi',
       author_email='ghobadimhd@gmail.com',
       url="https://github.com/ghobadimhd/syncme",
-      python_requires='==3.5.*',
+      python_requires='>=3.5',
       install_requires=['pyaml==17.*'],
       py_modules=['syncme'],
       entry_points={
