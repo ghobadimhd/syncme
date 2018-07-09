c74d9afbb0071a373fb8b4fbe8026c7670b4baba
mamad <ghobadimhd@gmail.com>
2018-07-10 01:45:58 +0430

    Critical bug fixed, a lot of code refactored

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from setuptools import setup
 
 setup(name='syncme',
-      version='0.1.4',
+      version='0.1.5',
       description='Simple rsync wrapper',
       keywords='sample setuptools development',
       author='Mohammad Ghobadi',
